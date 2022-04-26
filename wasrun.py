32802f90d2ddf7c07eb8429150d892223feba376
Alexander Nemtsev <mrcrazy696@gmail.com>
2022-04-26 22:35:08 +0300

    Добавил обработку методов с exeption

--- wasrun.py
+++ wasrun.py
@@ -4,14 +4,25 @@ class TestCase:
 
     def setUp(self):
         pass
+
     def tearDown(self):
         pass
 
     def run(self):
-        self.setUp()
-        method = getattr(self, self.name)
-        method()
+        result = TestResult()
+        result.testStarted()
+        try:
+            self.setUp()
+        except:
+            result.testFailed()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
+        return result
+
 
 class WasRun(TestCase):
     def __init__(self, name) -> None:
@@ -26,10 +37,51 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log = self.log + 'tearDown '
 
+    def testBrokenMethod(self):
+        raise Exception
+
+
+class TestResult:
+    def __init__(self) -> None:
+        self.runCount = 0
+        self.errorCount = 0
+
+    def testStarted(self):
+        self.runCount = + 1
+
+    def testFailed(self):
+        self.errorCount += 1
+
+    def summary(self):
+        return f'{self.runCount} run, {self.errorCount} failed'
+
+
 class TestCaseTest(TestCase):
+    def setUp(self):
+        raise Exception
+
     def testTemplateMethod(self):
         test = WasRun('testMethod')
         test.run()
         assert('setUp testMethod tearDown ' == test.log)
 
+    def testResult(self):
+        test = WasRun('testMethod')
+        result = test.run()
+        assert('1 run, 0 failed' == result.summary())
+
+    def testBrokenMethod(self):
+        test = WasRun('testBrokenMethod')
+        result = test.run()
+        assert('1 run, 1 failed' == result.summary())
+
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert('1 run, 1 failed' == result.summary())
+
+
 TestCaseTest('testTemplateMethod').run()
+TestCaseTest('testResult').run()
+TestCaseTest('testBrokenMethod').run()
